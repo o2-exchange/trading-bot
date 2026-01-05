113bea299bf1e2c2b37c2943c3ce022b27d8b7e8
rishabhkeshan <rishabhkeshan@gmail.com>
2026-01-05 21:09:14 +0530

    feat: added posthog analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { PostHogProvider } from 'posthog-js/react'
 import App from './App.tsx'
 import './index.css'
+
+const posthogOptions = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  defaults: '2025-11-30',
+} as const
 
 // Apply dark theme by default (matching o2 frontend)
 document.documentElement.classList.add('dark')
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <PostHogProvider apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY} options={posthogOptions}>
+      <App />
+    </PostHogProvider>
   </React.StrictMode>,
 )
 
